e21d17dd81621691d9c3dc4b2bbfb21cffe24554
Fabricio <ftrennepohl@gmail.com>
2022-03-29 20:44:36 -0300

    Update trabapl3.py

--- trabapl3.py
+++ trabapl3.py
@@ -22,14 +22,18 @@ def area_sob_curva():
         intervalos_esq.append(intervalos_esq[i-1] + delta_x)
         intervalos_dir.append(intervalos_dir[i-1] + delta_x) 
     
-    #Funcção aplicada aos extremos dos subintervalos
+    #Função aplicada aos extremos dos subintervalos
     soma_dir = 0
     soma_esq = 0
     for i in range(n):
         soma_esq += funcao(intervalos_esq[i]) * delta_x
         soma_dir += funcao(intervalos_dir[i]) * delta_x
-
-    print(f"A área está entre {soma_esq:.3f} e {soma_dir:.3f}")
+        
+    if soma_dir > soma_esq:
+        print(f"A área está entre {soma_esq:.3f} e {soma_dir:.3f}")
+    else:
+        print(f"A área está entre {soma_dir:.3f} e {soma_esq:.3f}")
 
 
 area_sob_curva()
+
